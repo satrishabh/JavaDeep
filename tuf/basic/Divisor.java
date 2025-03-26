e4c5b70c4b4fb4096ab2be7ca4e3915d12cf23b5
Rishabh Mishra <i.mishra.rishabh@gmail.com>
2025-03-26 10:32:11 +0530

    basic algorithms

--- tuf/basic/Divisor.java
+++ tuf/basic/Divisor.java
@@ -5,11 +5,20 @@ public class Divisor {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         int count = 0;
-        for(int i = 1; i <= num/2; i++) {
-            if(num % i == 0) {
-                count++;
+        for(int i = 1; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                // Add divisor i to the list
+               count++;
+
+                // Add the counterpart divisor
+                // if it's different from i
+                if (i != num / i) {
+                    // Add the counterpart
+                    // divisor to the list
+                    count++;
+                }
             }
         }
-        System.out.println("Number of divisors: " + (count+1));
+        System.out.println("Number of divisors: " + count);
     }
 }
