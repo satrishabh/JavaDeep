d5140037f8249b59c767c4738325c1e56978ffd5
Rishabh Mishra <i.mishra.rishabh@gmail.com>
2025-03-27 22:30:33 +0530

    Recursion Factorial

---
+++ tuf/recursion/Factorial.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class Factorial {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = scanner.nextInt();
+        System.out.println("Factorial of " + n + " is " + factorial(n));
+    }
+    private static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }else if(n == 1){
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
