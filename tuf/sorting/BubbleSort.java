46a650a3d2ecb12a9b8d6aded0a6d12e99a4d31c
Rishabh Mishra <i.mishra.rishabh@gmail.com>
2025-04-10 10:09:12 +0530

    add the bubble sorting

--- tuf/sorting/BubbleSort.java
+++ tuf/sorting/BubbleSort.java
@@ -1,5 +1,18 @@
-package JavaDeep.tuf.sorting;
-
 public class BubbleSort {
-    
+    public static void main(String[] args) {
+        int A[]={1,2,3,6,0,5};
+        int temp;
+        for(int i=A.length-1;i>=0;i--){
+            for(int j=0;j<i;j++){
+                if(A[j]>A[j+1]){
+                    temp=A[j];
+                    A[j]=A[j+1];
+                    A[j+1]=temp;
+                }
+            }
+        }
+        for(int number:A){
+            System.out.println(number);
+        }
+    }
 }
